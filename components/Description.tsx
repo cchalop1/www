f4db22fd67f84357902c3eda3beefe97ac6f11a7
cchalopin <clement.chalopin@gmail.com>
2022-08-04 21:29:25 +0200

    fix: description format

--- components/Description.tsx
+++ components/Description.tsx
@@ -1,10 +1,11 @@
 const Description = () => {
   return (
     <div className="mt-6 mb-6">
-      Hello my name is Clément i live in Paris. I am passionate about computer
+        {`Hello my name is Clément i live in Paris. I am passionate about computer
       development and I love learning new things. I am currently in my 5th year
       at Epitech Paris and I work freelance alongside my studies. I'm in love
-      with open source. ♥️
+      with open source. ♥`}
+      ️
     </div>
   );
 };
