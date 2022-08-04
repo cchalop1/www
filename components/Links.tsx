8abb43cbb7ab77b5d12e2cbe5a79c807f6d4f4c2
cchalopin <clement.chalopin@gmail.com>
2022-08-04 23:24:22 +0200

    fix: add twitter link

--- components/Links.tsx
+++ components/Links.tsx
@@ -6,13 +6,22 @@ const links = [
     url: "https://www.linkedin.com/in/cl%C3%A9ment-chalopin-355305178/",
     color: "#0a66c2",
   },
+  {
+    title: "Twitter",
+    url: "https://twitter.com/ChalopinClement",
+    color: "#1d9bf0",
+  },
   { title: "Github", url: "https://github.com/cchalop1/", color: "#454545" },
-  { title: "Resume", url: "https://drive.google.com/file/d/1X9-fWoXeATwYX1pRTHRg2HrCMqaeuDPf/view?usp=sharing", color: "#700700" },
+  {
+    title: "Resume",
+    url: "https://drive.google.com/file/d/1X9-fWoXeATwYX1pRTHRg2HrCMqaeuDPf/view?usp=sharing",
+    color: "#700700",
+  },
 ];
 
 const Links: NextPage = () => {
   return (
-    <div className="flex justify-evenly">
+    <div className="flex justify-evenly mt-4">
       {links.map((l, idx) => (
         <a
           key={idx}
